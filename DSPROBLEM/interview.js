64cd15f70a2de518d2bbe2e031811253eaca1bea
suhara beevi <sairasuhara12345@gmail.com>
2024-04-25 11:44:57 +0530

    question no 349

--- DSPROBLEM/interview.js
+++ DSPROBLEM/interview.js
@@ -32,14 +32,98 @@
 // let s="idi"
 // console.log(minimizedStringLength(s));
 
-var getCommon = function(nums1, nums2) {
-    const set = new Set(nums1);
-    console.log(set);
-  for (const n of nums2) {
-    if (set.has(n)) return n;
-  }
-  return -1;
-}
-let  nums1 = [1,3,2,6,1,3]
-let nums2 = [3,4,2]
-console.log(getCommon(nums1,nums2));
+// var getCommon = function(nums1, nums2) {
+//     const set = new Set(nums1);
+//     console.log(set);
+//   for (const n of nums2) {
+//     if (set.has(n)) return n;
+//   }
+//   return -1;
+// }
+// let  nums1 = [1,3,2,6,1,3]
+// let nums2 = [3,4,2]
+// console.log(getCommon(nums1,nums2));
+
+
+// divide the array
+
+// function dividearray(nums,k){
+
+// nums.sort((a,b)=>
+// //     a-b
+// // )
+// // let array =[]
+// // for(let i=2;i<nums.length;i=i+3){
+// //     // if((nums[i]-nums[i-1]>k) ||(nums[i]-nums[i-2]>k) ||(nums[i-1]-nums[i-2]>k)){
+// //         if ((nums[i - 1] - nums[i - 2] > k) || (nums[i] - nums[i - 1] > k) || ((nums[i] - nums[i - 2] > k))) {
+
+// //         return []
+// //     }else{
+// //         array.push(nums[i-2],nums[i-1],nums[i])
+// //     }
+// // }
+// // return array
+// // }
+// function dividearray(nums, k) {
+//     nums.sort((a, b) => a - b);
+
+//     let array = [];
+
+//     for (let i = 2; i < nums.length; i += 3) {
+//         if (
+//             nums[i] - nums[i - 1] > k ||
+//             nums[i] - nums[i - 2] > k ||
+//             nums[i - 1] - nums[i - 2] > k
+//         ) {
+//             return [];
+//         } else {
+//             array.push(nums[i - 2], nums[i - 1], nums[i]);
+//         }
+//     }
+
+//     return array;
+// }
+
+// // let nums = [1, 3, 4, 8, 7, 9, 3, 5, 1];
+// // let k = 2;
+// // console.log(dividearray(nums, k));
+
+// let nums = [1,3,4,8,7,9,3,5,1];
+// let  k = 2
+// console.log(dividearray(nums,k));
+
+// function divideArray (nums, k) {
+//     nums.sort((a, b) => a - b)
+//     const res = []
+//     for (i = 2; i < nums.length; i = i + 3) {
+//         if ((nums[i - 1] - nums[i - 2] > k) || (nums[i] - nums[i - 1] > k) || ((nums[i] - nums[i - 2] > k))) {
+//             return []
+//         } else {
+//             res.push([nums[i - 2], nums[i - 1], nums[i]])
+//         }
+//     }
+//     return res
+// };
+// let nums = [1,3,4,8,7,9,3,5,1];
+//  let  k = 2
+//  console.log(divideArray(nums,k));
+
+var intersection = function(nums1, nums2) {
+let Array1=[]
+    let newSet1= new Set(nums1)
+    let newSet2= new Set(nums2)
+    // if(newSet1.has(newSet2)){
+        for( item of newSet1){
+            if(newSet2.has(item)){
+                Array1.push(item)
+            }
+        }
+        return Array1
+
+    }
+
+
+let nums1 = [1,2,2,1];
+ let nums2 = [2,2];
+
+console.log(intersection(nums1,nums2));
